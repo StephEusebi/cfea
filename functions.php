327ce00ea64401d8555a2f00b221eddd40f210cb
Steph Eusebi <steph.eusebi@hotmail.com>
2019-02-10 14:57:08 -0500

    Added Foundations
    css file, js files, and enqueues to functions.php

--- functions.php
+++ functions.php
@@ -20,6 +20,13 @@ require get_template_directory() . '/inc/setup.php';
 function stepheusebi_scripts() {
 	wp_enqueue_style( 'cfea-theme-style', get_stylesheet_uri() );
 
+	// Foundation
+	wp_enqueue_script( 'what-input', get_template_directory_uri() . '/js/vendor/what-input.min.js', array('jquery'), '', true );
+	wp_enqueue_style( 'foundation-style', get_template_directory_uri() . '/css/vendor/foundation.min.css', array(), 'all' );
+	wp_enqueue_script( 'foundation-app', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true );
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/js/vendor/foundation.min.js', array('jquery'), '', true );
+
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
