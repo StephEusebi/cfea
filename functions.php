c130b77264e5f231412741edf0c6ef3d747bfa90
Steph Eusebi <steph.eusebi@hotmail.com>
2019-02-11 12:20:14 -0500

    functions.php worked on. added css and css reset

--- functions.php
+++ functions.php
@@ -20,11 +20,18 @@ require get_template_directory() . '/inc/setup.php';
 function stepheusebi_scripts() {
 	wp_enqueue_style( 'cfea-theme-style', get_stylesheet_uri() );
 
+	// APP CSS
+	wp_enqueue_style( 'css-app', get_template_directory_uri() . '/assets/css/app.css', array(), null);
+
+	// RESET CSS
+	wp_enqueue_style( 'reset-css', get_template_directory_uri() . '/assets/css/reset.css', array(), null);
+
 	// Foundation
 	wp_enqueue_script( 'what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.min.js', array('jquery'), '', true );
 	wp_enqueue_style( 'foundation-style', get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', array(), 'all' );
 	wp_enqueue_script( 'foundation-app', get_template_directory_uri() . '/assets/js/app.js', array('jquery'), '', true );
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/js/vendor/foundation.min.js', array('jquery'), '', true );
+
 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
