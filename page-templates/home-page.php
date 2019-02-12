563f767d8d26fae5494b2068ddc9d9e6a6bc451c
Steph Eusebi <steph.eusebi@hotmail.com>
2019-02-12 16:01:20 -0500

    Added mini bio with image and team bio/img
    filler bio and image. still waiting for image in bio

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -45,17 +45,65 @@ get_header();
 					<div class="cell vipr"><img src="/wp-content/themes/cfea/assets/img/vipr-logo.png" alt="vipr logo" /></div>
 				</div>
 
-				<div class="grid-x grid-padding-x">
-					<div class="cell large-3 large-offset-3 text-center">
-						<h2>Curis Medina</h2>
-						<h3>CEO, CFEA</h3>
-						<p>Mini Bio - Awards, Achievements</p>
-						<p>Over 15 years experience, nomiated.... how many fitness professionsal he taught</p>
+
+
+				<!-- container for curtis and mini bio -->	
+				<div class="grid-container grid-container-padded">	
+
+					<div class="grid-x grid-padding-x align-center">
+
+						<div class="cell large-4 small-12 text-right" >
+							<!-- line  -->
+							<div style="width: 100%; height: 0.3rem; background-color: #0a415e; margin-bottom: 1rem;"></div>
+							<p><strong>Curtis Medina</strong> | Certified Fitness ... | CEO, CFEA</p>
+							
+							<p>Mini Bio - Awards, Achievements<br>
+							Over 15 years experience, nomiated.... how many fitness professionsal he taught
+							<br>Why work with him?</p>
+
+							<p><a href="#" target="_blank"><em>Get Certified with Curtis</em></a></p>
+
+						</div>
+
+						<div class="cell large-8 small-12">							
+							<!-- image of curtis -->
+							<img src="/wp-content/themes/cfea/assets/img/headshot.png" alt="head shot of curtis" style="width: 880px;" />
+
+						</div>
 					</div>
 
-					<div class="cell large-6"><img src="/wp-content/themes/cfea/assets/img/headshot.png" alt="head shot of curtis" /></div>
+				</div><!-- grid-container end-->
 
-				</div>
+
+				<!-- container for curtis and mini bio -->	
+				<div class="grid-container grid-container-padded top-space">	
+
+					<div class="grid-x grid-padding-x align-center">
+
+						<div class="cell large-8 small-12">							
+							<!-- image of curtis -->
+							<img src="/wp-content/themes/cfea/assets/img/group.jpg" alt="head shot of curtis" style="width: 880px;" />
+
+						</div>
+
+						<div class="cell large-4 small-12" >
+							<!-- line  -->
+							<div style="width: 100%; height: 0.3rem; background-color: #0a415e; margin-bottom: 1rem;"></div>
+							<p><strong>Team info</strong></p>
+							
+							<p>team and program info</p>
+							<p>more team and program info. have about 4-5 lines of text and a all to action</p>
+
+							<p>more team and program info. have about 4-5 lines of text and a all to action more team and program info. have about 4-5 lines of text and a all to action</p>
+							<p><a href="#" target="_blank"><em>Get Certified!</em></a></p>
+
+						</div>
+
+					</div>
+
+				</div><!-- grid-container end-->
+
+
 
 			</div> <!-- grid-x end-->
 
