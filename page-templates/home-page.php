b13cbbb72db46fa5f685874a8678f39e73a3d5d6
Steph Eusebi <steph.eusebi@hotmail.com>
2019-02-11 21:38:59 -0500

    Worked on home-page and css

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -26,25 +26,38 @@ get_header();
 			<div class="grid-x">
 
 				<!-- top container holding h1 and buttpn -->
-				<div class="row large-8 large-offset-2">
+				<div class="row large-6 large-offset-3">
 
 					<!-- title -->
-					<h1 class="text-center blue">A leading provider of fitness certification and education in Canada</h1>
+					<h1 class="text-center blue top-space">A leading provider of fitness certification and education in Canada</h1>
 
 					<!-- button with button class because it links to internal pages -->
-					<a class="button float-center top-space" href="#">Get Cerfied Today!</a>
+					<a class="button float-center top-space" href="#"><h4>Get Cerfied Today!</h4></a>
 
 				</div>
+			
 
-				<!-- holding images -->
-				<div class="grid-x medium-up-2 large-up-4 bottom-space top-space">
+				<!-- group of images -->
+				<div class="grid-x medium-up-2 large-up-4 bottom-space top-space home-images-4">
 					<div class="cell corefx"><img src="/wp-content/themes/cfea/assets/img/corefx-logo.png" alt="corefx logo" /></div>
 					<div class="cell trigger"><img src="/wp-content/themes/cfea/assets/img/tp-logo.png" alt="trigger point logo" /></div>
 					<div class="cell firstaid"><img src="/wp-content/themes/cfea/assets/img/firstaid-logo.png" alt="canadian red cross logo" /></div>
 					<div class="cell vipr"><img src="/wp-content/themes/cfea/assets/img/vipr-logo.png" alt="vipr logo" /></div>
 				</div>
 
-			</div> <!-- offset end-->
+				<div class="grid-x grid-padding-x">
+					<div class="cell large-3 large-offset-3 text-center">
+						<h2>Curis Medina</h2>
+						<h3>CEO, CFEA</h3>
+						<p>Mini Bio - Awards, Achievements</p>
+						<p>Over 15 years experience, nomiated.... how many fitness professionsal he taught</p>
+					</div>
+
+					<div class="cell large-6"><img src="/wp-content/themes/cfea/assets/img/headshot.png" alt="head shot of curtis" /></div>
+
+				</div>
+
+			</div> <!-- grid-x end-->
 
 			
 		</main><!-- #main -->
