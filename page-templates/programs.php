f9694642b5154c8d7f8dd24745e11847e0f8eff0
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-02-12 17:59:48 -0500

    Updated programs.php
    Added information regarding different programs available. Added a class to the headers and paragraphs on the programs page to center align text.

--- page-templates/programs.php
+++ page-templates/programs.php
@@ -20,8 +20,40 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			
+			<h1>PROGRAMS</h1>
+			
+			<h2 class="cell large-12 progDesc">TriggerPoint</h2>
+			<p class="cell large-12 progDesc">TriggerPoint Performance is a patented brand that improves life by focusing on 
+			energizing people with the radical organization in Myofascial Compression techniques 
+			as well as branded goods that conserve, control increase the physical movement of human 
+			life-forms. The goal of TriggerPoint Performance is to discover inventive ways to which 
+			people can be inspired to attain the best degree of change using providing medicinal 
+			self-care products and teachings. Our aim is to deliver the greatest user-friendly mode 
+			of goods, training, and treatment available for anyone who needs a change in lifestyle, 
+			enhanced performance or typical good health. We would like people to manage their curative 
+			care. Discover more. Have better movement.</p>
+			
+			<h2 class="cell large-12 progDesc">COREFX</h2>
+			<p class="cell large-12 progDesc">COREFX asserts itself when it comes to fitness and training equipment. It is a guarantee 
+			to the best. For athletes looking to release their greatest capabilities and get motivated 
+			to prominence, it is the best answer. The trailblazing COREFX peak-performing lines of products 
+			that have undergone real life controlled tests are purposefully created to take peak performance 
+			to another level by delivering the most all-embracing area of strength and training tools.</p>
 
-
+			<p class="cell large-12 progDesc">COREFX has created new and technologically-advanced training methods so as to connect the disparity 
+			between persistent physical activity and authentic physical improvement. This permits athletes to 
+			exploit the full extensive possibilities of the company’s whole range of products. To complement 
+			athletes’ unrestrained commitment to exercise, COREFX has created a mutually beneficial connection 
+			between equipment and training. This is the genuine mystery behind attaining your Inner Strength.</p>
+			
+			<h2 class="cell large-12 progDesc">Canadian Red Cross</h2>
+			<p class="cell large-12 progDesc">Duration: Level A: 4 hours, Level C: 5 hours, Level HCP: 6 hours</p>
+			<p class="cell large-12 progDesc">Lecture series on mouth-to-mouth resuscitation (CPR) offers the needed expertise to identify and act 
+			in response to cardiac crisis and suffocation for babies, children and adults subject to the type of CPR 
+			chosen. The training also comprises the utilization of automated external defibrillator (AED) as well as 
+			guidance that is appropriate for place of work or broad appeal. In addition, there are cutting-edge CPR 
+			recommendations.</p>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
