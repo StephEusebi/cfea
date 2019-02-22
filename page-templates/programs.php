c60da4eb5129e9aefeb9d6a79fc083e22ea1a2e1
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-02-21 21:47:36 -0500

    created programs branch

--- page-templates/programs.php
+++ page-templates/programs.php
@@ -33,6 +33,33 @@ get_header();
 			of goods, training, and treatment available for anyone who needs a change in lifestyle, 
 			enhanced performance or typical good health. We would like people to manage their curative 
 			care. Discover more. Have better movement.</p>
+			<table class="progTable">
+				<tr>
+					<th>Program</th>
+					<th>Requirements</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 1</td>
+					<td>No equipment required</td>
+					<td>$219.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 1</td>
+					<td>TriggerPoint Grid</td>
+					<td>$259.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 2</td>
+					<td>No equipment required</td>
+					<td>$319.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 2</td>
+					<td>TriggerPoint Kit</td>
+					<td>$459.00 plus tax</td>
+				</tr>
+			</table>
 			
 			<h2 class="cell large-12 progDesc">COREFX</h2>
 			<p class="cell large-12 progDesc">COREFX asserts itself when it comes to fitness and training equipment. It is a guarantee 
@@ -47,6 +74,29 @@ get_header();
 			athletes’ unrestrained commitment to exercise, COREFX has created a mutually beneficial connection 
 			between equipment and training. This is the genuine mystery behind attaining your Inner Strength.</p>
 			
+			<table class="progTable">
+				<tr>
+					<th>Program</th>
+					<th>Requirements</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>COREFX Level 1</td>
+					<td>No equipment required</td>
+					<td>$259.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>COREFX Workshop</td>
+					<td>Grit and Gut Workshop</td>
+					<td>$59.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>COREFX Workshop</td>
+					<td>Power Play</td>
+					<td>$59.00 plus tax</td>
+				</tr>
+			</table>
+
 			<h2 class="cell large-12 progDesc">Canadian Red Cross</h2>
 			<p class="cell large-12 progDesc">Duration: Level A: 4 hours, Level C: 5 hours, Level HCP: 6 hours</p>
 			<p class="cell large-12 progDesc">Lecture series on mouth-to-mouth resuscitation (CPR) offers the needed expertise to identify and act 
@@ -54,6 +104,66 @@ get_header();
 			chosen. The training also comprises the utilization of automated external defibrillator (AED) as well as 
 			guidance that is appropriate for place of work or broad appeal. In addition, there are cutting-edge CPR 
 			recommendations.</p>
+
+			<table class="progTable">
+				<tr>
+					<th>Program</th>
+					<th>Requirements</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>CPR A with AED</td>
+					<td>$49.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>CPR C with AED</td>
+					<td>$55.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>Emergency First Aid and CPR with AED</td>
+					<td>$75.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>Standard First Aid and CPR with AED</td>
+					<td>$115.00 plus tax</td>
+				</tr>
+			</table>
+
+			<h1 class="cell large-12 progDesc">ViPR</h1>
+			<p class="cell large-12 progDesc">The disparity between movement and power exercises is linked by ViPR. It merges complete body 
+			movement with weight, thereby improving the vivacity, implementation along with overhauling the 
+			aspirations of customers and sportspeople. ViPR ushers in a new perception in fitness and athletic 
+			training – Loaded Movement Training.</p>
+
+			<p class="cell large-12 progDesc">Loaded Movement Training blends movements that are adapted for resilient exercises. Quickness and 
+			power originate from the body as it moves in a variety of focused undertakings with weight, just like 
+			back at the homestead.</p>
+
+			<p class="cell large-12 progDesc">ViPR was stimulated by farm youngsters who moved with weights on a daily basis and who were more powerful 
+			than any other contender in athletics. Using ViPR, we can turn out farm-toughened physique everywhere, sans 
+			setting foot on a farm, without having to carry out any hard work in an entertaining, harmless and efficient 
+			style.</p>
+
+			<p class="cell large-12 progDesc">As Michol Dalcourt, the creator of ViPR clarifies; the art of movement reinforces incorporating Loaded 
+			Movement Training by using ViPR into existing exercise procedures for all fitness objectives.</p>
+			
+			<table class="progTable">
+				<tr>
+					<th>Program</th>
+					<th>Requirements</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>ViPR</td>
+					<td>Loaded Movement Training</td>
+					<td>$225.00 plus tax</td>
+				</tr>
+			</table>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
