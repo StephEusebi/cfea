58fa0015b29536f0201f3b871fb47b530a30d575
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-02-21 22:20:38 -0500

    individual program pages
    corefx, redcross, and triggerPoint

---
+++ page-templates/redCross.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying all pages
+ * 
+ * Template Name: RedCross Page
+ * Template Post Type: page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cfea
+ */
+
+// imports the header
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<h2 class="cell large-12 progDesc">Canadian Red Cross</h2>
+			<p class="cell large-12 progDesc">Duration: Level A: 4 hours, Level C: 5 hours, Level HCP: 6 hours</p>
+			<p class="cell large-12 progDesc">Lecture series on mouth-to-mouth resuscitation (CPR) offers the needed expertise to identify and act 
+			in response to cardiac crisis and suffocation for babies, children and adults subject to the type of CPR 
+			chosen. The training also comprises the utilization of automated external defibrillator (AED) as well as 
+			guidance that is appropriate for place of work or broad appeal. In addition, there are cutting-edge CPR 
+			recommendations.</p>
+
+			<table>
+				<tr>
+					<th>Certification</th>
+					<th>Details</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>CPR A with AED</td>
+					<td>$49.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>CPR C with AED</td>
+					<td>$55.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>Emergency First Aid and CPR with AED</td>
+					<td>$75.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>Canadian Red Cross</td>
+					<td>Standard First Aid and CPR with AED</td>
+					<td>$115.00 plus tax</td>
+				</tr>
+			</table>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// imports the footer
+get_footer();
---
+++ page-templates/triggerPoint.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying all pages
+ * 
+ * Template Name: TriggerPoint Page
+ * Template Post Type: page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cfea
+ */
+
+// imports the header
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<h2 class="cell large-12 progDesc">TriggerPoint</h2>
+			<p class="cell large-12 progDesc">TriggerPoint Performance is a patented brand that improves life by focusing on 
+			energizing people with the radical organization in Myofascial Compression techniques 
+			as well as branded goods that conserve, control increase the physical movement of human 
+			life-forms. The goal of TriggerPoint Performance is to discover inventive ways to which 
+			people can be inspired to attain the best degree of change using providing medicinal 
+			self-care products and teachings. Our aim is to deliver the greatest user-friendly mode 
+			of goods, training, and treatment available for anyone who needs a change in lifestyle, 
+			enhanced performance or typical good health. We would like people to manage their curative 
+			care. Discover more. Have better movement.</p>
+
+			<table>
+				<tr>
+					<th>Certification</th>
+					<th>Requirements</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 1</td>
+					<td>No Equipment Required</td>
+					<td>$219.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 1</td>
+					<td>TriggerPoint Grid</td>
+					<td>$259.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 2</td>
+					<td>No Equipment Required</td>
+					<td>$319.00 plus tax</td>
+				</tr>
+				<tr>
+					<td>TriggerPoint Level 2</td>
+					<td>TriggerPoint Kit</td>
+					<td>$459.00 plus tax</td>
+				</tr>
+			</table>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// imports the footer
+get_footer();
