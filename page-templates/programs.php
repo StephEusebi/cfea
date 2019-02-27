9b46f46a2e9948933995b78717c0608cfa945b9f
Cassie Doyle <cassie.doyle@live.ca>
2019-02-27 00:28:40 -0500

    updated accordions

--- page-templates/programs.php
+++ page-templates/programs.php
@@ -19,6 +19,41 @@ get_header();
 
 ?>
 
+<?php 
+
+if (function_exists('get_field')) {
+	$programs = get_field('programs');
+
+	foreach($programs as $program){
+
+		$name = $program['name'];
+		$description = $program['description'];
+		$button = $program['button'];
+?>
+
+<ul class="accordion plAccordian" data-accordion>
+				<li class="accordion-item accordianStyle" data-accordion-item>
+					<!-- Accordion tab title -->
+					<a href="#" class="accordion-title plAccordianTitle"><?php echo$name; ?></a>
+
+					<!-- Accordion tab content -->
+					<div class="accordion-content" data-tab-content>
+					<p><?php echo$description; ?></p>
+					<a  class="button">Learn More<?php echo$button; ?></a>
+					</div>
+				</li>
+		
+
+			</ul>
+
+
+		<?php
+
+	}
+}
+
+?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			
@@ -27,24 +62,41 @@ get_header();
 			<p class="programInfo">At CFEA we have a number of different programs to help you reach all of your 
 			fitness goals!</p>
 
+<!-- 
 			<ul class="accordion plAccordian" data-accordion>
 				<li class="accordion-item accordianStyle" data-accordion-item>
-					<!-- Accordion tab title -->
+				
+					<a href="#" class="accordion-title plAccordianTitle"><?php echo$name; ?></a>
+
+					
+					<div class="accordion-content" data-tab-content>
+					<p><?php echo$description; ?></p>
+					<a  class="button">Learn More<?php echo$button; ?></a>
+					</div>
+				</li>
+		
+
+			</ul> -->
+
+
+
+			<!-- <ul class="accordion plAccordian" data-accordion>
+				<li class="accordion-item accordianStyle" data-accordion-item>
+				
 					<a href="#" class="accordion-title plAccordianTitle">COREFX</a>
 
-					<!-- Accordion tab content -->
 					<div class="accordion-content" data-tab-content>
 					<p>COREFX has created new and technologically-advanced training methods so as to connect the disparity 
 			between persistent physical activity and authentic physical improvement.</p>
 					<a href="corefx" class="button">Learn More</a>
 					</div>
 				</li>
-  <!-- ... -->
+
 				<li class="accordion-item accordianStyle" data-accordion-item>
-					<!-- Accordion tab title -->
+				
 					<a href="#" class="accordion-title plAccordianTitle">TRIGGERPOINT</a>
 
-					<!-- Accordion tab content -->
+				
 					<div class="accordion-content" data-tab-content>
 					<p>TriggerPoint Performance is a patented brand that improves life by focusing on 
 			energizing people with the radical organization in Myofascial Compression techniques 
@@ -55,29 +107,29 @@ get_header();
 				</li>
 
 				<li class="accordion-item accordianStyle" data-accordion-item>
-					<!-- Accordion tab title -->
+				
 					<a href="#" class="accordion-title plAccordianTitle">CANADIAN RED CROSS</a>
 
-					<!-- Accordion tab content -->
+				
 					<div class="accordion-content" data-tab-content>
 					<p>CPR/AED</p>
-					<a href="redcross.php" class="button">Learn More</a>
+					<a href="redcross" class="button">Learn More</a>
 					</div>
 				</li>
 
 				<li class="accordion-item accordianStyle" data-accordion-item>
-					<!-- Accordion tab title -->
+				
 					<a href="#" class="accordion-title plAccordianTitle">VIPR</a>
 
-					<!-- Accordion tab content -->
+				
 					<div class="accordion-content" data-tab-content>
 					<p>The disparity between movement and power exercises is linked by ViPR. 
 			It merges complete body movement with weight, thereby improving the vivacity, implementation along with 
 			overhauling the aspirations of customers and sportspeople.</p>
-					<a href="vipr.php" class="button">Learn More</a>
+					<a href="vipr" class="button">Learn More</a>
 					</div>
 				</li>
-			</ul>
+			</ul> -->
 			
 		</main><!-- #main -->
 	</div><!-- #primary -->
