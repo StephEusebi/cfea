d0670844e5196fb92e7610b1b700f4f13031ad9f
Cassie Doyle <cassie.doyle@live.ca>
2019-03-04 13:17:45 -0500

    create acfs for triggerpoint

--- page-templates/triggerPoint.php
+++ page-templates/triggerPoint.php
@@ -19,13 +19,19 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div class="grid-x grid-margin-x">
+	<div class="large-offset-2 ">
 
-			<img src="/wp-content/themes/cfea/assets/img/trigPoint.jpg" alt="triggerPoint logo" class="cell large-12" id="trigLogo" />
-
-			<!-- <h2 class="cell large-12 progHead">TriggerPoint</h2> -->
-			<p class="cell large-12 progDesc">TriggerPoint Performance is a patented brand that improves life by focusing on 
+			<!-- banner at top of page -->
+		<div class="cell large-12">
+				<img src="/wp-content/themes/cfea/assets/img/triggerpointbanner.png" alt="triggerPoint banner" id="trigBannerss" />
+		</div>
+				
+					
+		<!-- program description -->
+		<div class="grid-x grid-margin-x">
+				
+				<p class="cell large-10 progDesc">TriggerPoint Performance is a patented brand that improves life by focusing on 
 			energizing people with the radical organization in Myofascial Compression techniques 
 			as well as branded goods that conserve, control increase the physical movement of human 
 			life-forms. The goal of TriggerPoint Performance is to discover inventive ways to which 
@@ -33,44 +39,78 @@ get_header();
 			self-care products and teachings. Our aim is to deliver the greatest user-friendly mode 
 			of goods, training, and treatment available for anyone who needs a change in lifestyle, 
 			enhanced performance or typical good health. We would like people to manage their curative 
-			care. Discover more. Have better movement.</p>
+			care. Discover more. Have better movement.
+				</p>
 
-			<table class="programTables">
-				<tr>
-					<th>Certification</th>
-					<th>Requirements</th>
-					<th>Price</th>
-				</tr>
-				<tr>
-					<td>TriggerPoint Level 1</td>
-					<td>No Equipment Required</td>
-					<td>$219.00 plus tax</td>
-				</tr>
-				<tr>
-					<td>TriggerPoint Level 1</td>
-					<td>TriggerPoint Grid</td>
-					<td>$259.00 plus tax</td>
-				</tr>
-				<tr>
-					<td>TriggerPoint Level 2</td>
-					<td>No Equipment Required</td>
-					<td>$319.00 plus tax</td>
-				</tr>
-				<tr>
-					<td>TriggerPoint Level 2</td>
-					<td>TriggerPoint Kit</td>
-					<td>$459.00 plus tax</td>
-				</tr>
-			</table>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</div>
 
+			<div class="grid-x grid-margin-x ">
+					
+				
+	<?php 
+
+		if (function_exists('get_field')) {
+			$programs = get_field('programs');
+
+			foreach($programs as $program){
+				$name = $program['name'];
+				$description = $program['description'];
+				$button = $program['button'];
+				$cost = $program['cost'];
+				$program_image = $program['program_image'];
+			
+	?>
+
+			<!-- group of smaller boxes for workshop -->
+			<div class="cell large-5 ">
+			
+							<img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" class="rcImg"/>
+							
+								
+							<ul class="accordion plAccordian" data-accordion data-allow-all-closed="true">
+								<li class="accordion-item accordianStyle" data-accordion-item>
+									<!-- Accordion tab title -->
+									<a href="#" class="accordion-title pRCAccordianTitle"><?php echo$name; ?></a>
+
+									<!-- Accordion tab content -->
+									<div class="accordion-content" data-tab-content>
+									<p><?php echo$description; ?></p>
+									<p><?php echo$cost; ?></p>
+									<button  class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
+
+									</div>
+								</li>
+							</ul>
+			</div>		
+						
+						
+	<?php
+
+		}
+	}
+
+	?>
+
+			</div>
+	</div>
+
+
+		<!-- insert video -->
 		<div class="large-offset-3 "> 
 			<div class="cell large-8 rcVideo">
 			<iframe width="798" height="449" src="https://www.youtube.com/embed/GYpCABKHsFw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</div>
 		</div>
+
+		<!-- bottom page logo -->
+		<div class="cell large-8">
+		<img src="/wp-content/themes/cfea/assets/img/trigPoint.jpg" alt="triggerPoint logo" class="cell large-12" id="trigLogo2" />
+		</div>
+	</div>
+
+
+
 
 <?php
 // imports the footer
