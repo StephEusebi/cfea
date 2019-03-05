566f88b34063fac0797bc4ea389a3ad369bd5074
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-03-04 22:20:06 -0500

    Created ACFs for ViPR page and added content

--- page-templates/triggerPoint.php
+++ page-templates/triggerPoint.php
@@ -67,7 +67,6 @@ get_header();
 			
 							<img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" class="rcImg"/>
 							
-								
 							<ul class="accordion plAccordian" data-accordion data-allow-all-closed="true">
 								<li class="accordion-item accordianStyle" data-accordion-item>
 									<!-- Accordion tab title -->
@@ -75,10 +74,9 @@ get_header();
 
 									<!-- Accordion tab content -->
 									<div class="accordion-content" data-tab-content>
-									<p><?php echo$description; ?></p>
-									<p><?php echo$cost; ?></p>
-									<button  class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
-
+										<p><?php echo$description; ?></p>
+										<p><?php echo$cost; ?></p>
+										<button  class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
 									</div>
 								</li>
 							</ul>
@@ -99,13 +97,13 @@ get_header();
 		<!-- insert video -->
 		<div class="large-offset-3 "> 
 			<div class="cell large-8 rcVideo">
-			<iframe width="798" height="449" src="https://www.youtube.com/embed/GYpCABKHsFw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<iframe width="798" height="449" src="https://www.youtube.com/embed/GYpCABKHsFw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</div>
 		</div>
 
 		<!-- bottom page logo -->
 		<div class="cell large-8">
-		<img src="/wp-content/themes/cfea/assets/img/trigPoint.jpg" alt="triggerPoint logo" class="cell large-12" id="trigLogo2" />
+			<img src="/wp-content/themes/cfea/assets/img/trigPoint.jpg" alt="triggerPoint logo" class="cell large-12" id="trigLogo2" />
 		</div>
 	</div>
 
