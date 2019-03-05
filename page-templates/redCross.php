acac24cef3aad3bf574784b495c967667ca5414b
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-03-04 22:26:49 -0500

    Revert "Merge branch 'programs3' into develop"
    This reverts commit 45cb30d2f4945eb70929ab935e2775ed79d2fe4e.

--- page-templates/redCross.php
+++ page-templates/redCross.php
@@ -22,26 +22,24 @@ get_header();
 
 
 <div class="grid-x grid-margin-x">
-	<div class="large-offset-2">
+			<div class="large-offset-2">
 			
-		<!-- main logo -->
-		<div class="cell large-8">
-			<img src="/wp-content/themes/cfea/assets/img/crcLogo.jpg" alt="red cross logo" class="cell large-12" id="redCrossLogo" />
-		</div>
+				<div class="cell large-8">
+					<img src="/wp-content/themes/cfea/assets/img/crcLogo.jpg" alt="red cross logo" class="cell large-12" id="redCrossLogo" />
+				</div>
 
-		<div class="grid-x grid-margin-x">
+				<div class="grid-x grid-margin-x">
 			
-			<!-- program description -->
-			<p class="cell large-10 progDesc">Lecture series on mouth-to-mouth resuscitation (CPR) offers the needed expertise to identify and act 
+					<p class="cell large-10 progDesc">Lecture series on mouth-to-mouth resuscitation (CPR) offers the needed expertise to identify and act 
 						in response to cardiac crisis and suffocation for babies, children and adults subject to the type of CPR 
 						chosen. The training also comprises the utilization of automated external defibrillator (AED) as well as 
 						guidance that is appropriate for place of work or broad appeal. In addition, there are cutting-edge CPR 
 						recommendations.
-			</p>
+					</p>
 	
-		</div>
+				</div>
 
-		<div class="grid-x grid-margin-x">
+				<div class="grid-x grid-margin-x">
 				
 			
 
@@ -58,28 +56,67 @@ if (function_exists('get_field')) {
 		$program_image = $program['program_image'];
 		
 ?>
-			<!-- create boxes to hold page content -->
-			<div class="cell large-5 ">
-					<!-- box cover image -->
-					<img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" class="rcImg"/>
-									
-					<ul class="accordion plAccordian" data-accordion data-allow-all-closed="true">
+
+<div class="cell large-5 ">
+		
+						<img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" class="rcImg"/>
+						
+							
+						<ul class="accordion plAccordian" data-accordion data-allow-all-closed="true">
 							<li class="accordion-item accordianStyle" data-accordion-item>
 								<!-- Accordion tab title -->
 								<a href="#" class="accordion-title pRCAccordianTitle"><?php echo$name; ?></a>
 
 								<!-- Accordion tab content -->
 								<div class="accordion-content" data-tab-content>
-									<p><?php echo$description; ?></p>
-									<p><?php echo$cost; ?></p>
-									<button  class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
+								<p><?php echo$description; ?></p>
+								<p><?php echo$cost; ?></p>
+								<button  class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
 
 								</div>
 							</li>
-					</ul>
-			</div>		
+						</ul>
+						</div>		
 					
-<?php
+<!-- 
+					<div class="cell large-5 programBox">
+					<table>
+						<tr>
+							<td>CPR C with AED</td>
+							<td>$55.00 plus tax</td>
+						</tr>
+					</table>
+					<button class="programButton button">SIGN UP NOW</button>
+					</div>
+
+					<div class="cell large-5 programBox">
+					<table>
+						<tr>
+							<td>Emergency First Aid and CPR with AED</td>
+							<td>$75.00 plus tax</td>
+						</tr>
+					</table>
+					<button class="programButton button">SIGN UP NOW</button>
+					</div>
+
+					<div class="cell large-5 programBox">
+					<table>
+						<tr>
+							<td>Standard First Aid and CPR with AED</td>
+							<td>$115.00 plus tax</td>
+						</tr>
+					</table>
+					<button class="programButton button">SIGN UP NOW</button>
+					</div>
+
+					</div> -->
+			
+			
+	
+
+
+
+		<?php
 
 	}
 }
@@ -87,9 +124,9 @@ if (function_exists('get_field')) {
 ?>
 
 
-		</div> 
+		</div>
 	</div>
-		<!-- insert video -->
+
 		<div class="large-offset-3 "> 
 			<div class="cell large-8 rcVideo">
 			<iframe width="798" height="449" src="https://www.youtube.com/embed/oZOrRJJt5Tk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- page-templates/vipr.php
+++ page-templates/vipr.php
@@ -19,90 +19,49 @@
 get_header();
 ?>
 
-<div class="grid-x grid-margin-x">
-	<div class="large-offset-2 ">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-			<!-- banner at top of page -->
-		<div class="cell large-8">
-			<img src="/wp-content/themes/cfea/assets/img/vipr_banner.png" alt="vipr banner" id="trigBannerss" />
-		</div>
+			<img src="/wp-content/themes/cfea/assets/img/vipr.jpg" alt="vipr logo" class="cell large-12" id="viprLogo" />
 
-			<!-- <img src="/wp-content/themes/cfea/assets/img/vipr.jpg" alt="vipr logo" class="cell large-12" id="viprLogo" /> -->
-
-		<div class="grid-x grid-margin-x">
 			<!-- <h2 class="cell large-12 progHead">ViPR</h2> -->
-			<p class="cell large-10 progDesc">The disparity between movement and power exercises is linked by ViPR. 
+			<p class="cell large-12 progDesc">The disparity between movement and power exercises is linked by ViPR. 
 			It merges complete body movement with weight, thereby improving the vivacity, implementation along with 
 			overhauling the aspirations of customers and sportspeople. ViPR ushers in a new perception in fitness and 
 			athletic training – Loaded Movement Training.</p>
-			<p class="cell large-10 progDesc">Loaded Movement Training blends movements that are adapted for resilient 
+			<p class="cell large-12 progDesc">Loaded Movement Training blends movements that are adapted for resilient 
 			exercises. Quickness and power originate from the body as it moves in a variety of focused undertakings with 
 			weight, just like back at the homestead.</p>
-			<p class="cell large-10 progDesc">ViPR was stimulated by farm youngsters who moved with weights on a daily 
+			<p class="cell large-12 progDesc">ViPR was stimulated by farm youngsters who moved with weights on a daily 
 			basis and who were more powerful than any other contender in athletics. Using ViPR, we can turn out 
 			farm-toughened physique everywhere, sans setting foot on a farm, without having to carry out any hard work in 
 			an entertaining, harmless and efficient style.</p>
-			<p class="cell large-10 progDesc">As Michol Dalcourt, the creator of ViPR clarifies; the art of movement 
+			<p class="cell large-12 progDesc">As Michol Dalcourt, the creator of ViPR clarifies; the art of movement 
 			reinforces incorporating Loaded Movement Training by using ViPR into existing exercise procedures for all 
 			fitness objectives.</p>
-		</div>
 
-	<div class="grid-x grid-margin-x ">
-					
-				
-					<?php 
-				
-						if (function_exists('get_field')) {
-							$programs = get_field('programs');
-				
-							foreach($programs as $program){
-								$name = $program['name'];
-								$description = $program['description'];
-								$button = $program['button'];
-								$cost = $program['cost'];
-								$program_image = $program['program_image'];
-							
-					?>
-				
-							<!-- group of smaller boxes for workshop -->
-							<div class="cell large-5">
-							
-											<img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" class="rcImg"/>
-											
-											<ul class="accordion plAccordian" data-accordion data-allow-all-closed="true">
-												<li class="accordion-item accordianStyle" data-accordion-item>
-													<!-- Accordion tab title -->
-													<a href="#" class="accordion-title pRCAccordianTitle"><?php echo$name; ?></a>
-				
-													<!-- Accordion tab content -->
-													<div class="accordion-content" data-tab-content>
-														<p><?php echo$description; ?></p>
-														<p><?php echo$cost; ?></p>
-														<button  class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
-													</div>
-												</li>
-											</ul>
-							</div>		
-										
-										
-					<?php
-				
-						}
-					}
-				
-					?>
-				
-							</div>
-					</div>
-				
+			<table class="programTables">
+				<tr>
+					<th>Certification</th>
+					<th>Details</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+					<td>ViPR</td>
+					<td>Loaded Movement Training</td>
+					<td>$225.00 plus tax</td>
+				</tr>
+			</table>
 
-	<!-- <div class="large-offset-3 "> 
-		<div class="cell large-8 rcVideo">
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<div class="large-offset-3 "> 
+			<div class="cell large-8 rcVideo">
 			<iframe width="798" height="449" src="https://www.youtube.com/embed/AWVC2cQqRqo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
 		</div>
-	</div> -->
 
-</div>
 <?php
 // imports the footer
 get_footer();
