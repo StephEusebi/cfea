4ec93fe661e2c4a1a112898de50f83dfee97c246
Cassie Doyle <cassie.doyle@live.ca>
2019-03-05 13:39:39 -0500

    Revert "Merge branch 'develop' into programs3"
    This reverts commit 104ce35dc68b9a3d268e841b52eb446f315f7ea1.

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 	<div class="grid-x">
           <div class="cell small-12 medium-12 large-6 logoDiv">
 		<!-- Logo Image -->
-		<a href="#"><img class="logoImg" src="/wp-content/themes/cfea/assets/img/logo.png" alt="logo" /></a>
+		<img class="logoImg" src="/wp-content/themes/cfea/assets/img/logo.png" alt="logo" />
 
 		  </div>
 		  <!--Menu (thats opens to accordion)-->
