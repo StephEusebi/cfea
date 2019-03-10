1c755c3b42f9408878294dbc6023b4b382329a78
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-03-09 20:59:18 -0500

    added canfitpro content

--- page-templates/canfitpro.php
+++ page-templates/canfitpro.php
@@ -29,17 +29,22 @@ get_header();
 
             <!-- <h2 class="cell large-12 progHead">COREFX</h2> -->
             <div class="grid-x grid-margin-x">
-
+                <p class="cell large-10 small-10 progDesc">Whether you are new to the fitness industry looking to become certified or a seasoned veteran looking for advanced certification and continuing education we can help. </p>
                 <p class="cell large-10 small-10 progDesc">Canfitpro is the most recognized provider of fitness certification in Canada today. Through our partnership with Allied Education we are able to offer a wide variety canfitpro certification courses including:</p>
                 <ul class="cell large-10 small-10 progDesc">
-                    <li>Personal Trainer Specialist (PTS)</li>
-                    <li>Fitness Instructor Specialist (FIS)</li>
-                    <li>Healthy Eating & Weight Loss Coach (HWL)</li>
-                    <li>Pre & Post Natal Specialist (PFS)</li>
-                    <li>First Aid & Cardio Pulmonary Resuscitation (First Aid &CPR)</li>
+                    <li style="font-weight:bold;">Personal Trainer Specialist (PTS)</li>
+                        <p>Personal training is a rapidly growing profession, which can make it difficult to sort through the options and choose the best avenue to become qualified to train clients. canfitpro provides the answer with the Personal Training Specialist (PTS) certification. You will learn the foundational skills necessary to confidently and competently train apparently healthy individuals. A canfitpro certified PTS is qualified to assess, design, and deliver individual and small group training programs in a health club, home, and workplace setting.
+                        Canada’s leading personal training certification designation, the canfitpro Personal Training Specialist program provides comprehensive program content and a wide range of practical skills that will ensure success to those who hold the designation. canfitpro has collaborated with Human Kinetics Publishing Inc. to create a multimedia learning experience for personal trainers that is second to none!</p>
+                    <li style="font-weight:bold;">Fitness Instructor Specialist (FIS)</li>
+                        <p>The role of a Fitness Instructor Specialist has evolved far beyond being a basic “aerobic instructor”. As a group exercise expert, you will assist club members in a fun and motivating setting. A qualified Fitness Instructor Specialist can lead groups in various formats of cardiovascular conditioning, muscle strength and endurance training, stretching and relaxation, or a calming cool-down. A canfitpro certified FIS is knowledgeable about different types of music, understands how to use music in a group setting, keeps current on the latest fitness trends and is able to give generalized advice about healthy living.</p>
+                    <li style="font-weight:bold;">Healthy Eating & Weight Loss Coach (HWL)</li>
+                        <p>For the majority of people, exercise alone is not enough to create lasting weight loss and health.</p>
+                        <p>Knowing that rates of overweight and obesity are increasing across Canada, canfitpro has created the Healthy Eating & Weight Loss (HWL) Coach program. Choose to gain the certification and work with clients or learn simply to empower your own healthy choices.</p>
+                        <p>The Healthy Eating & Weight Loss Coach program delivers information in a straightforward manner so you can confidently help clients make better choices; more often, lose weight, and lead healthier lives!</p>
+                        <p>Nutrition information is constantly evolving and being able to decipher this information and apply it to fat loss strategies is essential for success. This program will allow you to understand healthy eating, the value of regular activity and ways to reduce body fat. This course will provide excellent strategies and tools for fitness professionals to help them educate others about how to build a healthier body.</p>
+                    <li style="font-weight:bold;">Pre & Post Natal Specialist (PFS)</li>
+                    <li style="font-weight:bold;">First Aid & Cardio Pulmonary Resuscitation (First Aid &CPR)</li>
                 </ul>
-
-                <p class="cell large-12 small-10 progDesc">Whether you are new to the fitness industry looking to become certified or a seasoned veteran looking for advanced certification and continuing education we can help. </p>
 
             </div>
         </div>
