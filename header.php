64655e62c9336229c047325597150859c517d41c
hannahleslie <hannah.leslie@hotmail.com>
2019-03-11 13:28:18 -0400

    ACF logo image

--- header.php
+++ header.php
@@ -31,7 +31,12 @@
 	<div class="grid-x">
           <div class="cell small-12 medium-12 large-6 logoDiv">
 		<!-- Logo Image -->
-		<a href="#"><img class="logoImg" src="/wp-content/themes/cfea/assets/img/logo.png" alt="logo" /></a>
+
+		<?php if( get_field('image') ): ?>
+
+<img src="<?php the_field('image'); ?>" />
+
+<?php endif; ?>
 
 		  </div>
 		  <!--Menu (thats opens to accordion)-->
