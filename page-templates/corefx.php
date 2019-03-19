815a07e9c950abb1e34c2fb6a25b9fa77b31396f
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-03-19 13:11:35 -0400

    changed layout of corefx page

--- page-templates/corefx.php
+++ page-templates/corefx.php
@@ -24,20 +24,43 @@ get_header();
         <div class="large-offset-2 small-offset-1">
 
             <!-- banner at top of page -->
-            <div class="cell large-10">
-                <img src="/wp-content/themes/cfea/assets/img/corefxbanner.png" alt="corefx banner" id="corefxBanner" />
-            </div>
+            <?php 
 
-            <!-- program description -->
-            <div class="grid-x grid-margin-x">
+if (function_exists('get_field')) {
+$descriptions = get_field('descriptions');
 
-                <p class="cell large-10 progDesc small-10">COREFX asserts itself when it comes to fitness and training equipment. It is a guarantee to the best. For athletes looking to release their greatest capabilities and get motivated to prominence, it is the best answer. The trailblazing COREFX peak-performing lines of products that have undergone real life controlled tests are purposefully created to take peak performance to another level by delivering the most all-embracing area of strength and training tools.
-                </p>
+foreach($descriptions as $description){
+    $top_desc = $description['top_desc'];
+    $program_banner = $description['program_banner'];
 
-                <p class="cell large-10 progDesc small-10">COREFX has created new and technologically-advanced training methods so as to connect the disparity between persistent physical activity and authentic physical improvement. This permits athletes to exploit the full extensive possibilities of the company’s whole range of products. To complement athletes’ unrestrained commitment to exercise, COREFX has created a mutually beneficial connection between equipment and training. This is the genuine mystery behind attaining your Inner Strength.
-                </p>
 
-            </div>
+?>
+
+
+    <div class="large-offset-2 small-offset-1">
+
+        <!-- banner at top of page -->
+        <div class="cell large-12">
+            <img src="<?php echo$program_banner; ?>" alt="triggerPoint banner" id="trigBanner" />
+        </div>
+
+        <!-- program description -->
+        <div class="grid-x grid-margin-x">
+
+<!-- program description -->
+<div class="cell large-10 small-10 progDesc"><?php echo$top_desc; ?>
+</div>
+
+</div>
+
+        <?php
+        
+
+    }
+}
+
+?>
+
 
             <div class="grid-x grid-margin-x">
 
@@ -56,32 +79,33 @@ get_header();
 
 	?>
 
-                            <!-- group of smaller boxes for workshop -->
-                            <div class="cell large-3 small-10 corefxContainer">
 
-                                <img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" />
+            <!-- create boxes to hold page content -->
+            <div class="cell large-4 small-10">
+                <!-- box cover image -->
+                <img src="<?php echo$program_image; ?>" alt="<?php echo $image['alt']; ?>" />
 
-                                <ul class="accordion plAccordian" data-accordion data-allow-all-closed="true">
-                                    <li class="accordion-item accordianStyle" data-accordion-item>
-                                        <!-- Accordion tab title -->
-                                        <a href="#" class="accordion-title pRCAccordianTitle">
-                                            <?php echo$name; ?>
-                                        </a>
+            
+                        <!-- Accordion tab title -->
+                        <h2>
+                            <?php echo$name; ?>
+                        </h2>
 
-                                        <!-- Accordion tab content -->
-                                        <div class="accordion-content" data-tab-content>
-                                            <p>
-                                                <?php echo$description; ?>
-                                            </p>
-                                            <p>
-                                                <?php echo$cost; ?>
-                                            </p>
-                                            <button class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
+                        <!-- Accordion tab content -->
+                        <div class="accordion-content" data-tab-content>
+                            <p>
+                                <?php echo$description; ?>
+                            </p>
+                            <p>
+                                <?php echo$cost; ?>
+                            </p>
+                            <button class=" button" href="<?php echo$button; ?>">REGISTER NOW</button>
 
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
+                        </div>
+                
+            </div>
+
+                          
 
             <?php
 
