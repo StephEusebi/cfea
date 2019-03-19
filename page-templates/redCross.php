0cd604fe8057d4a397dc822364306203d22d108b
Amanda Jackson <amanda.jackson@dcmail.ca>
2019-03-19 19:07:56 -0400

    small change to program layouts

--- page-templates/redCross.php
+++ page-templates/redCross.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying all pages
  * 
- * Template Name: RedCross Page
+ * Template Name: COREFX Page
  * Template Post Type: page
  *
  * This is the template that displays all pages by default.
@@ -18,144 +18,117 @@
 // imports the header
 get_header();
 ?>
+
 <div class="grid-container">
-    <div class="grid-x grid-margin-x">
-        <div class="cell large-offset-2 small-offset-1">
-
-            <!-- banner at top of page -->
-            <?php 
-
-    if (function_exists('get_field')) {
-    $descriptions = get_field('descriptions');
-
-    foreach($descriptions as $description){
-        $top_desc = $description['top_desc'];
-        $program_banner = $description['program_banner'];
-
-
-                ?>
-
-
-<!-- BANNER AND TOP DESCRIPTION -->
-   <div class="grid-container">
-        <div class="grid-x grid-margin-x">
-
-                <!-- banner at top of page -->
-                <div class="cell large-12">
-                    <img src="<?php echo$program_banner; ?>" alt="corefx banner" id="corefxBanner" />
-                </div>
-            
-                <!-- program description -->
-                <div class="cell large-10 small-10 progDesc"><?php echo$top_desc; ?></div>
-
-                <div class="cell small-10 medium-10 large-10 dividerBar"></div>  
-        </div>
-   
-        
-
     
-        <?php
         
-
-    }
-}
-
-?>
-<!-- END OF BANNER AND TOP DESCRIPTION -->
-
-
-            <div class="grid-x grid-margin-x">
-
-    <?php 
-
-		if (function_exists('get_field')) {
-			$programs = get_field('programs');
-
-			foreach($programs as $program){
-				$name = $program['name'];
-				$description = $program['description'];
-				$button = $program['button'];
-				$cost = $program['cost'];
-                $program_image = $program['program_image'];
-                $ytvideo = $program['ytvideo'];
-
-	?>
-
-                
-               
-                <!-- create boxes to hold page content -->
-               
-                <!-- box cover image -->
-               
-
-            
-                        <!-- Accordion tab title -->
-                        <div class="cell large-10 small-10 coreContent">
-                            <h2>
-                                <?php echo$name; ?>
-                            </h2>
-                        </div>
-                       
-                        <div class="cell large-10 small-10 coreContent">
-                           
-                             <!-- program image -->
-                             <img src="<?php echo$program_image; ?>" alt="<?php echo$program_image['alt']; ?>" id="<?php echo$program_image['id']; ?>" />
-                             <p id="coreDesc">
-                                <?php echo$description; ?>
-                            </p>
-                        </div>
-
-                         <div class="cell large-10 small-10 coreContent">
-                            <p>
-                                <?php echo$cost; ?>
-                            </p>
-                            <button class="button" href="<?php echo$button; ?>">REGISTER NOW</button>    
-                        </div>  
-
-                        <div class="cell small-10 medium-10 large-10 dividerBar"></div>                
-
-            <?php
-
-		}
-	}
-
-	?>
-
-            </div>
-
-        </div>
-
+                <!-- banner at top of page -->
         <?php 
+        if (function_exists('get_field')) {
+        $descriptions = get_field('descriptions');
 
-if (function_exists('get_field')) {
-$programs = get_field('programs');
-
-    foreach($programs as $program){
-     
-        $ytvideo = $program['ytvideo'];
-
-?>
-
-<div class="grid-x coreContent">
-    <div class="cell large-10 small-10 progVideo">
-      
-        <?php echo$ytvideo; ?>
-      
-    </div>
-    </div>
-
-    <?php
-                
-
-            }
-        }
-        
+        foreach($descriptions as $description){
+            $top_desc = $description['top_desc'];
+            $program_banner = $description['program_banner'];
         ?>
 
 
+    <!-- BANNER AND TOP DESCRIPTION -->
+            <div class="grid-x grid-margin-x">
+                    <!-- banner at top of page -->
+                    <div class="cell large-12">
+                        <img src="<?php echo$program_banner; ?>" alt="corefx banner" id="corefxBanner" />
+                    </div>
+                    <!-- program description -->
+                    <div class="cell large-10 small-10 progDesc"><?php echo$top_desc; ?></div>
+                    <div class="cell small-10 medium-10 large-10 dividerBar"></div>  
+            </div>
+    
+            
+            <?php
+            
 
-  </div>
-  </div>
+        }
+    }
+
+    ?>
+    <!-- END OF BANNER AND TOP DESCRIPTION -->
+
+
+        <div class="grid-x grid-margin-x">
+
+        <?php 
+
+            if (function_exists('get_field')) {
+                $programs = get_field('programs');
+
+                foreach($programs as $program){
+                    $name = $program['name'];
+                    $description = $program['description'];
+                    $button = $program['button'];
+                    $cost = $program['cost'];
+                    $program_image = $program['program_image'];
+                    $ytvideo = $program['ytvideo'];
+
+        ?>
+
+                            <!-- Accordion tab title -->
+                            <div class="cell large-10 small-10 coreContent">
+                                <h2>
+                                    <?php echo$name; ?>
+                                </h2>
+                            </div>
+                        
+                            <div class="cell large-10 small-10 coreContent">
+                                <!-- program image -->
+                                <img src="<?php echo$program_image; ?>" alt="<?php echo$program_image['alt']; ?>" id="<?php echo$program_image['id']; ?>" />
+                                <p id="coreDesc">
+                                    <?php echo$description; ?>
+                                </p>
+                            </div>
+
+                            <div class="cell large-10 small-10 coreContent">
+                                <p>
+                                    <?php echo$cost; ?>
+                                </p>
+                                <button class="button" href="<?php echo$button; ?>">REGISTER NOW</button>    
+                            </div>  
+
+                            <div class="cell small-10 medium-10 large-10 dividerBar"></div>                
+
+                <?php
+
+            }
+        }
+
+        ?>
+
+        </div>
+
+           
+
+            <?php 
+
+    if (function_exists('get_field')) {
+    $programs = get_field('programs');
+
+        foreach($programs as $program){
+            $ytvideo = $program['ytvideo'];
+    ?>
+
+        <div class="grid-x coreContent">
+            <div class="cell large-10 small-10 progVideo">
+                <?php echo$ytvideo; ?>
+            </div>
+        </div>
+        <?php
+                }
+            }
+        ?>
+
+
+ 
+</div>
 
     <?php
 // imports the footer
