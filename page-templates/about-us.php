ad3ca4d9245ab9d89d58b5f7e737f2bad3ac9c01
hannahleslie <hannah.leslie@outlook.com>
2019-03-19 19:07:33 -0400

    about us comments

--- page-templates/about-us.php
+++ page-templates/about-us.php
@@ -36,28 +36,36 @@ The CFEA’s mandate is simple.  To bring together the most recognized education
 </div>
 	</div>
 
-	<div class="grid-x grid-padding-x">
-	<div class="cell large-12 ourTeam">
+	<div class="grid-x grid-padding-x"><!-- Grid Container Team Cards  -->
+	<div class="cell large-12 ourTeam"><!-- Cell Container -->
   <?php 
 
+/* ACF functions */
 if (function_exists('get_field')) {
 	$team = get_field('team');
-
+/* ACF functions from ACF "team" */
 	foreach($team as $team){
 
-		$member_name = $team['member_name'];
+		$member_name = $team['member_name']; 
 		$member_image = $team['member_image'];
 		$member_programs = $team['member_programs'];
 		$memeber_location = $team['memeber_location'];
 		$member_phone = $team['member_phone'];
 		$member_email = $team['member_email'];
 ?>
-			<div class="teamMembers">
-       <p class="member_name"><?php echo$member_name;?></p>
+		<!-- team members card info -->
+		<div class="teamMembers">
+			<!-- team members name-->
+       		<p class="member_name"><?php echo$member_name;?></p>
+	  		 <!-- team members image -->
 			 <img class="teamMemberImg" src="<?php echo$member_image;?>">
+			 <!-- team members programs -->
 			 <p  class="member_programs"> <img src="<?php echo get_template_directory_uri() . '/assets/img/listDot.svg'; ?>" class="teamIcons" />	<?php echo$member_programs;?></p>
+			<!-- team members location -->
 			 <p class="memeber_location"><img src="<?php echo get_template_directory_uri() . '/assets/img/location.svg'; ?>" class="teamIcons" />	 <?php echo$memeber_location;?></p>
+			<!-- team members phone number -->
 			 <p class="member_phone"> <img src="<?php echo get_template_directory_uri() . '/assets/img/phone.svg'; ?>" class="teamIcons" />	<?php echo$member_phone;?></p>
+			 <!-- team members mail -->
 			 <p class="member_email"> <img src="<?php echo get_template_directory_uri() . '/assets/img/email.svg'; ?>" class="teamIcons" />	<?php echo$member_email;?></p>
 
 	</div>
