078dd9b03088bb65d28ba789bd0f725563889ce7
Erin <erin.cronin@dcmail.ca>
2019-03-22 14:52:03 -0400

    removed footer form
    we decided to remove the form

--- footer.php
+++ footer.php
@@ -17,140 +17,96 @@
 
 	<footer id="colophon" class="site-footer">
 	
+		<!-- footer container -->
 		<div class="grid-x">
-
-			<!-- logo grid -->
-			<div class="cell large-2 medium-2 small-12 logo">
-
-				<?php
-					if ( has_custom_logo() ) {
-						echo get_custom_logo();
-					}else{
-						echo 'no logo';
-					}
-				?>
-			
-			</div>
-
-			<!-- footer navagation grid -->
-			<div class="cell large-6 medium-6 small-12 footerNav">
-				
-				<!-- php call for the footer nav -->
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'footer_nav',
-						'menu_id'        => 'footer-menu',
-					) );
-				?>
-
-				<!-- php call for the program nav -->
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'program_nav',
-						'menu_id'        => 'program-menu',
-					) );
-				?>
-
-				<!-- div for group the contact info together -->
+			<!-- container for all content in the footer -->
+			<div class="cell large-12 medium-12 small-12">
+				<!-- logo div -->
+				<div class="logo">
+					<?php
+						if ( has_custom_logo() ) {
+							echo get_custom_logo();
+						}else{
+							echo 'no logo';
+						}
+					?>
+				</div>
+				<!-- navagation div -->
+				<div class="footerNav">
+					<!-- php call for the footer nav -->
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footer_nav',
+							'menu_id'        => 'footer-menu',
+						) );
+					?>
+					<!-- php call for the program nav -->
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'program_nav',
+							'menu_id'        => 'program-menu',
+						) );
+					?>
+				</div>
+				<!-- div the contact information -->
 				<div class="contactInfo">
-					<!-- phone number -->
+					<!-- customizer for the phone number -->
 					<?php
-					if ( get_theme_mod( 'cfea_footer_contact_number' ) ) : ?>
-						<p><i class="fas fa-phone"></i><?php echo get_theme_mod( 'cfea_footer_contact_number' ); ?></p>
-					
+						if ( get_theme_mod( 'cfea_footer_contact_number' ) ) : ?>
+						<p><i class="fas fa-phone"></i><?php echo get_theme_mod( 'cfea_footer_contact_number' );
+					?>
+						</p>
 					<?php
-					else : ?>
- 
-					<p>CFEA Contact Number</p>
- 
-					<?php endif; ?>
-
-					<!-- email -->
+						else :
+					?>
+						<!-- shows up if no number is put in the footer customizer -->
+						<p>CFEA Contact Number</p>
+					<?php 
+						endif;
+					?>
+					<!-- customizer for the email -->
 					<?php
-					if ( get_theme_mod( 'cfea_footer_contact_email' ) ) : ?>
-						<p> <i class="fas fa-envelope"></i><?php echo get_theme_mod( 'cfea_footer_contact_email' ); ?></p>
-					
+						if ( get_theme_mod( 'cfea_footer_contact_email' ) ) : ?>
+						<p> <i class="fas fa-envelope"></i><?php echo get_theme_mod( 'cfea_footer_contact_email' ); 
+					?>
+						</p>
 					<?php
-					else : ?>
- 
-					<p>CFEA Contact Email</p>
- 
-					<?php endif; ?>
+						else :
+					?>
+						<!-- shows up if no email is put in the footer customizer -->
+						<p>CFEA Contact Email</p>
+					<?php 
+						endif; 
+					?>
 				</div>
-				<!-- social media icons and links -->
+				<!-- social media icons div -->
 				<div class="socialMediaIcons">
-
-
-				<!-- customizer for social media -->
-
-				<!-- facebook -->
-				<?php 
-					if(get_theme_mod( 'cfea_facebook_url' )) { ?>
-
-						<a target="_blank" href="<?php echo get_theme_mod ('cfea_facebook_url') ?> " ><i class="fab fa-facebook-square fbIcon"></i></a>
-
-						<?php
-					
-					}
-				?>
-				<!-- instagram -->
-				<?php 
-					if(get_theme_mod( 'cfea_instagram_url' )) { ?>
-			
-
-						<a target="_blank" href="<?php echo get_theme_mod ('cfea_instagram_url') ?> " ><i class="fab fa-instagram instagramIcon"></i></a>
-				
-
-						<?php
-					
-					}
-				?>
-
-				<!-- twitter -->
-
-				<?php 
-					if(get_theme_mod( 'cfea_twitter_url' )) { ?>
-
-						<a target="_blank" href="<?php echo get_theme_mod ('cfea_twitter_url') ?> " ><i class="fab fa-twitter twitterIcon"></i></a>
-
-						<?php
-					
-					}
-				?>
-				 
-				</div>
-
-			
-			</div>
-
-			<!-- socialmedia and news letter form grid -->
-			<div class="cell large-4 medium-4 small-12 socialMedia">
-	
-				<!-- news letter form -->
-				<div class="newsLetterForm">
-
-					<!-- title of form -->
-					<?php
-					if ( get_theme_mod( 'cfea_footer_form_name' ) ) : ?>
-						<h2><?php echo get_theme_mod( 'cfea_footer_form_name' ); ?></h2>
-						<?php
-					else : ?>
- 
-					<p>CFEA Form Name</p>
- 
-					<?php endif; ?>
-					
-					<?php echo do_shortcode('[ws_form id="3"]'); ?>
-
-				</div>
-
-				
-			</div>
-
-		</div>
-
+					<!-- customizer for social media -->
+					<!-- facebook -->
+					<?php 
+						if(get_theme_mod( 'cfea_facebook_url' )) { ?>
+							<a target="_blank" href="<?php echo get_theme_mod ('cfea_facebook_url') ?> " ><i class="fab fa-facebook-square fbIcon"></i></a>
+							<?php
+						}
+					?>
+					<!-- instagram -->
+					<?php 
+						if(get_theme_mod( 'cfea_instagram_url' )) { ?>
+							<a target="_blank" href="<?php echo get_theme_mod ('cfea_instagram_url') ?> " ><i class="fab fa-instagram instagramIcon"></i></a>
+							<?php
+						}
+					?>
+					<!-- twitter -->
+					<?php 
+						if(get_theme_mod( 'cfea_twitter_url' )) { ?>
+							<a target="_blank" href="<?php echo get_theme_mod ('cfea_twitter_url') ?> " ><i class="fab fa-twitter twitterIcon"></i></a>
+							<?php
+						}
+					?>
+				</div><!-- closes the social media div -->
+			</div><!-- closes the main footer div -->
+		</div><!-- closes the grid-x div -->
 	</footer><!-- #colophon -->
-
 	</div><!-- #page -->
 
 <?php wp_footer(); ?>
