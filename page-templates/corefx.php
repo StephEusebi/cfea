b2a571f4aee542942a0fe86bca3aff7e1f781d76
Cassie Doyle <cassie.doyle@live.ca>
2019-03-22 23:26:36 -0400

    corefx conditional statements update

--- page-templates/corefx.php
+++ page-templates/corefx.php
@@ -20,7 +20,10 @@ get_header();
 ?>
 
 <div class="grid-container">
-    
+
+
+
+    <!-- BANNER AND TOP DESCRIPTION -->    
 <div class="grid-x grid-margin-x">
         <!-- banner at top of page -->
         <?php 
@@ -28,24 +31,32 @@ get_header();
         $descriptions = get_field('descriptions');
 
         foreach($descriptions as $description){
+            
+            $top_desc = $description['top_desc'];
             if (!empty ($top_desc)) {
-                $top_desc = $description['top_desc'];
+              
+                ?>
+          
+                           <!-- program description -->
+                    <div class="large-centered columns large-offset-1 small-centered small-offset-1 small-10 progDesc"><?php echo $top_desc; ?></div>
+                    <div class="cell small-10 small-offset-1 medium-10 large-centered dividerBar"></div>  
+                <?php 
             }
+
+            $program_banner = $description['program_banner'];
             if (!empty ($program_banner)) {
-                $program_banner = $description['program_banner'];
+                ?>
+                <!-- banner at top of page -->
+                    <div class="large-centered columns large-offset-1 small-centered small-offset-1 small-10">
+                        <img src="<?php echo $program_banner; ?>" alt="corefx banner" id="corefxBanner" class="large-centered columns coreContent" />
+                    </div>
+                <?php 
             }
         ?>
 
-
-    <!-- BANNER AND TOP DESCRIPTION -->
            
-                    <!-- banner at top of page -->
-                    <div class="large-centered columns large-offset-1 small-centered small-offset-1 small-10">
-                    <img src="<?php echo$program_banner; ?>" alt="corefx banner" id="corefxBanner" class="large-centered columns coreContent" />
-                  </div>
-                    <!-- program description -->
-                    <div class="large-centered columns large-offset-1 small-centered small-offset-1 small-10 progDesc"><?php echo$top_desc; ?></div>
-                    <div class="cell small-10 small-offset-1 medium-10 large-centered dividerBar"></div>  
+                
+            
              </div>
     
             
@@ -69,49 +80,53 @@ get_header();
                 $programs = get_field('programs');
                 $counter = 0;
                 foreach($programs as $program){
+                    $name = $program['name'];
                     if (!empty ($name)) {
-                        $name = $program['name'];
+                        ?> 
+                        <!-- Accordion tab title -->
+                        <div class="large-centered columns small-10 coreContent">
+                            <h2>
+                                <?php echo $name; ?>
+                            </h2>
+                        </div>
+                    <?php
                     } 
-                    if (!empty ($description)) {
-                        $description = $program['description'];
-                    }
-                    if (!empty ($button)) {
-                        $button = $program['button'];
-                    }
-                    if (!empty ($cost)) {
-                        $cost = $program['cost'];
-                    }
-                    if (!empty ($program_image)) {
-                        $program_image = $program['program_image'];
-                    }
-                    if (!empty ($ytvideo)) {
-                        $ytvideo = $program['ytvideo'];
-                    }
-
-        ?>
-
-
-                            <!-- Accordion tab title -->
-                            <div class="large-centered columns small-10 coreContent">
-                                <h2>
-                                    <?php echo$name; ?>
-                                </h2>
-                            </div>
-                        
+                    $description = $program['description'];
+                    $program_image = $program['program_image'];
+                    if (!empty ($description) || !empty ($program_image)) {
+                        ?>
                             <div class="large-centered columns small-10 coreContent">
                                 <!-- program image -->
                                 <img src="<?php echo $program_image; ?>" alt="" id="<?php echo $counter; ?>" />
                                 <p id="coreDesc">
-                                    <?php echo$description; ?>
+                                    <?php echo $description; ?>
                                 </p>
                             </div>
+                        <?php
+                    }
+                    $button = $program['button'];
+                    $cost = $program['cost'];
+                    if (!empty ($button) || !empty ($cost)) {
+                       ?>
 
                             <div class="large-centered columns small-10 coreContent">
                                 <p>
-                                    <?php echo$cost; ?>
+                                    <?php echo $cost; ?>
                                 </p>
-                                <button class="button" href="<?php echo$button; ?>">REGISTER NOW</button>    
-                            </div>  
+                                <button class="button" href="<?php echo $button; ?>">REGISTER NOW</button>    
+                            </div> 
+                       <?php
+                    }
+            
+              
+
+        ?>
+
+
+                       
+                        
+                        
+ 
 
                             <div class="cell small-10 medium-10 large-centered small-offset-1 dividerBar"></div>                
 
@@ -132,16 +147,19 @@ get_header();
     $programs = get_field('programs');
 
         foreach($programs as $program){
+            $ytvideo = $program['ytvideo'];
             if (!empty ($ytvideo)) {
-                $ytvideo = $program['ytvideo'];
-            }
-    ?>
+               ?>
 
-        <div class="grid-x coreContent">
-            <div class="large-centered columns small-10 progVideo">
-                <?php echo$ytvideo; ?>
-            </div>
-        </div>
+                    <div class="grid-x coreContent">
+                        <div class="large-centered columns small-10 progVideo">
+                            <?php echo $ytvideo; ?>
+                        </div>
+                    </div>
+        <?php
+            }
+        ?>
+
         <?php
                 }
             }
